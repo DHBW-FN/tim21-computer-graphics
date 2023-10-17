95db2025e2b6b063970c40781695b1aca9e45c2e
Tobias Goetz <contact@tobiasgoetz.com>
2023-10-17 15:12:07 +0200

    feat(init): Added lighting

--- src/main.js
+++ src/main.js
@@ -28,18 +28,19 @@ const velocity = new THREE.Vector3(0, 0, 0);
 
 scene.add(controls.getObject());
 
-// Create a directional light to act as the sun
-const sunLight = new THREE.DirectionalLight(0xffffff, 1);
-sunLight.position.set(0, -100, 200);
-sunLight.castShadow = true;
-scene.add(sunLight);
-
 // Loader
 const loader = new GLTFLoader();
 loader.load(
-  "/assets/models/scene.gltf",
+  "/assets/models/eiffel.glb",
   function (gltf) {
     scene.add(gltf.scene);
+
+    scene.traverse((object) => {
+      if (object instanceof THREE.Light) {
+        object.intensity = object.intensity * 0.001; // Reduce intensity by half
+      }
+    });
+
     animate();
   },
   undefined,
