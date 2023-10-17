ce03359ef71777d2c1005d1178d5bd60087ef2b8
Tobias Goetz <contact@tobiasgoetz.com>
2023-10-17 12:31:58 +0200

    feat(init): Added control explanations

--- src/control.js
+++ src/control.js
@@ -1,14 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
   const toggleButton = document.getElementById("toggleButton");
   const controls = document.getElementById("controls");
+  const controlsExplanationHeading = document.getElementById(
+    "controlsExplanationHeading",
+  );
 
   toggleButton.addEventListener("click", function (event) {
     event.stopPropagation();
     if (controls.classList.contains("hidden")) {
       controls.classList.remove("hidden");
+      controlsExplanationHeading.classList.remove("hidden");
       toggleButton.innerHTML = "&#9654;";
     } else {
       controls.classList.add("hidden");
+      controlsExplanationHeading.classList.add("hidden");
       toggleButton.innerHTML = "&#9664;";
     }
   });
