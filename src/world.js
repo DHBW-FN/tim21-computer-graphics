39c62276e9e3c94a2142946c83cd8f47ffcc9f8e
Tobias Goetz <contact@tobiasgoetz.com>
2023-10-18 12:03:49 +0200

    feat(init): Added comments to world class

--- src/world.js
+++ src/world.js
@@ -5,6 +5,7 @@ import EiffelTower from "./eiffeltower";
 
 export default class World {
   constructor() {
+    // Initialize the scene, renderer, camera, and controls
     this.scene = new THREE.Scene();
     this.renderer = new THREE.WebGLRenderer();
     this.camera = new THREE.PerspectiveCamera(
@@ -18,17 +19,24 @@ export default class World {
       this.renderer.domElement,
     );
     this.moveSpeed = 1;
+
+    // Create a drone and add it to the scene
     this.drone = new Drone(this.camera, this.renderer.domElement);
     this.drone.addToScene(this.scene);
     this.drone.addEventListeners();
+
+    // Create an array to store objects in the scene
     this.objects = [];
+
+    // Add objects to the scene
     this.objects.push(new EiffelTower(this.scene));
 
+    // Set up renderer
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(this.renderer.domElement);
 
+    // Set up scene background and camera position
     this.scene.background = new THREE.Color(0xaaaaaa);
-
     this.camera.position.set(0, 0, 100);
     this.camera.lookAt(0, 50, 0);
   }
@@ -36,10 +44,12 @@ export default class World {
   animate() {
     requestAnimationFrame(() => this.animate());
 
+    // Apply movements and update position of the drone
     this.drone.moveForward(this.drone.velocity.z);
     this.drone.moveRight(this.drone.velocity.x);
     this.drone.updatePosition();
 
+    // Render the scene
     this.renderer.render(this.scene, this.camera);
   }
 }
