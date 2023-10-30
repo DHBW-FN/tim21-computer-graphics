3ed482c0be42ec885cefb408354714c44de88e3a
Tobias Goetz <contact@tobiasgoetz.com>
2023-10-30 16:45:37 +0100

    feat(modeling): Refactor various code

--- src/scene/world.js
+++ src/scene/world.js
@@ -5,17 +5,46 @@ import ModelLoader from "../helpers/modelloader";
 
 export default class World {
   constructor() {
+    this.lights = [];
+
     // Initialize the scene, renderer, and objects
     this.scene = new THREE.Scene();
     this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+
+    // Add lights to the scene
+    this.addLights();
 
     // Create a drone and add it to the scene
     this.drone = new Drone();
     this.drone.addToScene(this.scene);
 
-    // Lights
-    this.lights = [];
+    // Set up scene background and camera position
+    this.scene.background = new THREE.Color(0xaaaaaa);
 
+    // Create and add model to the scene
+    this.modelLoader = new ModelLoader();
+    this.modelLoader.load("/assets/models/world/World.gltf").then((model) => {
+      this.scene.add(model);
+    });
+
+    // Set up camera
+    this.camera = this.drone.camera;
+    this.camera.position.set(400, 300, 150);
+    this.camera.lookAt(400, 0, -300);
+  }
+
+  animate() {
+    requestAnimationFrame(() => this.animate());
+
+    this.drone.updatePosition();
+
+    // Render the scene
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  addLights() {
     // Add sun
     this.sun = new DirectionalLight(0xffffff, 10);
     this.sun.position.set(200, 600, 200);
@@ -28,40 +57,6 @@ export default class World {
     // Add lights to scene
     this.lights.forEach((light) => {
       this.scene.add(light);
-    });
-
-    // Create and add objects to the scene
-    this.modelLoader = new ModelLoader();
-    this.addBase();
-
-    // Set up renderer
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(this.renderer.domElement);
-
-    // Set up scene background and camera position
-    this.scene.background = new THREE.Color(0xaaaaaa);
-
-    // Get the camera from the Drone class
-    this.camera = this.drone.camera;
-    this.camera.position.set(400, 300, 150);
-    this.camera.lookAt(400, 0, -300);
-  }
-
-  animate() {
-    requestAnimationFrame(() => this.animate());
-
-    // Apply movements and update the position of the drone
-    this.drone.moveForward(this.drone.velocity.z);
-    this.drone.moveRight(this.drone.velocity.x);
-    this.drone.updatePosition();
-
-    // Render the scene
-    this.renderer.render(this.scene, this.camera);
-  }
-
-  addBase() {
-    this.modelLoader.load("/assets/models/world/World.gltf").then((model) => {
-      this.scene.add(model);
     });
   }
 }
