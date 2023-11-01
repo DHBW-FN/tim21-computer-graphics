459f64147947352821fb4a6716d3f9a8c33fe484
Tobias Goetz <contact@tobiasgoetz.com>
2023-11-01 16:49:23 +0100

    feat(cameras): Add stationary and debug cameras

--- src/scene/world.js
+++ src/scene/world.js
@@ -6,6 +6,7 @@ import ModelLoader from "../helpers/modelloader";
 export default class World {
   constructor() {
     this.lights = [];
+    this.cameras = {};
 
     // Initialize the scene, renderer, and objects
     this.scene = new THREE.Scene();
@@ -19,6 +20,9 @@ export default class World {
     // Create a drone and add it to the scene
     this.drone = new Drone();
     this.drone.addToScene(this.scene);
+    this.cameras.drone = this.drone.camera;
+    this.cameras.drone.position.set(370, 1, -230);
+    this.cameras.drone.lookAt(615, 0, -230);
 
     // Set up scene background and camera position
     this.scene.background = new THREE.Color(0xaaaaaa);
@@ -29,19 +33,29 @@ export default class World {
       this.scene.add(model);
     });
 
-    // Set up camera
-    this.camera = this.drone.camera;
-    this.camera.position.set(400, 300, 150);
-    this.camera.lookAt(400, 0, -300);
+    // Add debug camera
+    this.cameras.debug = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.cameras.debug.position.set(370, 1, -230);
+    this.cameras.debug.lookAt(615, 100, -230);
+
+    // Add stationary camera
+    this.cameras.stationary = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.cameras.stationary.position.set(370, 1, -230);
+    this.cameras.stationary.lookAt(615, 100, -230);
+
+    // Set the initial camera
+    this.activeCamera = this.cameras.drone;
   }
 
   animate() {
     requestAnimationFrame(() => this.animate());
 
-    this.drone.updatePosition();
+    if (this.activeCamera === this.cameras.drone) {
+      this.drone.updatePosition();
+    }
 
     // Render the scene
-    this.renderer.render(this.scene, this.camera);
+    this.renderer.render(this.scene, this.activeCamera);
   }
 
   addLights() {
