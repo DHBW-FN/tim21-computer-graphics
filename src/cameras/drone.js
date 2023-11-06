a051d25e173761e18a901c920fd13b7098762b59
Tobias Goetz <contact@tobiasgoetz.com>
2023-11-06 10:02:06 +0100

    feat(cameras): Add cycling and resetting of cameras and a snackbar for status

--- src/cameras/drone.js
+++ src/cameras/drone.js
@@ -2,14 +2,19 @@ import { PointerLockControls } from "three/examples/jsm/controls/PointerLockCont
 import * as THREE from "three";
 
 export default class Drone {
-  constructor() {
+  constructor(world) {
+    this.world = world;
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     this.controls = new PointerLockControls(this.camera, document.body);
     this.moveSpeed = 1;
     this.velocity = new THREE.Vector3(0, 0, 0);
-    this.controls.getObject().position.set(0, 0, 100);
-    this.controls.getObject().lookAt(0, 50, 0);
+    this.setStartPosition();
     this.addEventListeners();
+  }
+
+  setStartPosition() {
+    this.camera.position.set(370, 1, -230);
+    this.camera.lookAt(615, 0, -230);
   }
 
   addToScene(scene) {
@@ -20,10 +25,10 @@ export default class Drone {
     document.addEventListener("keydown", (event) => this.onKeyDown(event));
     document.addEventListener("keyup", (event) => this.onKeyUp(event));
     document.addEventListener("click", () => {
-      if (this.controls.isLocked) {
-        this.controls.unlock();
-      } else {
+      if (!this.controls.isLocked && this.world.activeCamera === this.camera) {
         this.controls.lock();
+      } else {
+        this.controls.unlock();
       }
     });
     this.controls.addEventListener("lock", () => this.onPointerLock());
@@ -76,12 +81,12 @@ export default class Drone {
 
   // eslint-disable-next-line class-methods-use-this
   onPointerLock() {
-    // Hide UI or pause game
+    this.world.snackbar.show("Controls locked", 1000);
   }
 
   // eslint-disable-next-line class-methods-use-this
   onPointerUnlock() {
-    // Show UI or unpause game
+    this.world.snackbar.show("Controls unlocked", 1000);
   }
 
   moveForward(value = this.velocity.z) {
