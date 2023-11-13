c1fd4331f0b9189e99d9ae694f37a5fd06ca2947
philippkust <philipp.kuest@t-online.de>
2023-11-13 14:19:03 +0100

    added day and night background

--- src/components/control.js
+++ src/components/control.js
@@ -11,7 +11,8 @@ function initControls() {
 
   let rotateInterval;
 
-  let isNight = false;
+  const date = new Date();
+  let isNight = date.getHours() > 18 || date.getHours() < 6;
 
   toggleButton.addEventListener("click", (event) => {
     event.stopPropagation();
@@ -28,7 +29,12 @@ function initControls() {
 
   dayNightToggle.addEventListener("click", () => {
     isNight = !isNight;
-    // TODO: Add code to toggle night mode
+    // TODO: Add light depending on Background
+    if (isNight) {
+      world.setNightBackground();
+    } else {
+      world.setDayBackground();
+    }
   });
 
   startPositionButton.addEventListener("click", () => {
