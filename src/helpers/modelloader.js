78a00b660c4a5b6ab536dd21a8e1063a9c4e815f
Tobias Goetz <contact@tobiasgoetz.com>
2023-11-15 19:35:24 +0100

    feat(models): Rework modelloader.js

--- src/helpers/modelloader.js
+++ src/helpers/modelloader.js
@@ -1,94 +1,73 @@
 // eslint-disable-next-line import/extensions,import/no-unresolved
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-import { BufferGeometry, Mesh, MeshBasicMaterial } from "three";
+import { BufferGeometry, Group, Mesh, MeshBasicMaterial } from "three";
 import { acceleratedRaycast, computeBoundsTree, disposeBoundsTree } from "three-mesh-bvh";
 
 export default class ModelLoader {
-  static boundingObjectSuffix = "-boundingObject";
-
   static showBoundingBox = false;
-
-  static ignoreCollisionObjects = [];
-
-  static ignoreBoundingBoxDisplay = ModelLoader.ignoreCollisionObjects + [];
 
   constructor() {
     this.loader = new GLTFLoader();
-  }
 
-  load(modelPath, collidableObjects) {
-    return new Promise((resolve, reject) => {
-      this.loader.load(
-        modelPath,
-        (gltf) => {
-          const modifiedGltf = { ...gltf };
-
-          modifiedGltf.scene.children = gltf.scene.children.map((child) => {
-            if (!(child instanceof Mesh)) {
-              return child;
-            }
-
-            if (ModelLoader.showBoundingBox && !ModelLoader.ignoreBoundingBoxDisplay.includes(child.name)) {
-              return ModelLoader.getBoundingObject(child);
-            }
-
-            return child;
-          });
-
-          // Add the bounding objects to the list of collidable objects
-          collidableObjects.push(
-            ...modifiedGltf.scene.children
-              .map((child) => {
-                if (ModelLoader.ignoreCollisionObjects.includes(child.name)) {
-                  return null;
-                }
-
-                return ModelLoader.getBoundingObject(child);
-              })
-              .filter(Boolean),
-          );
-
-          resolve(modifiedGltf.scene);
-        },
-        (xhr) => {
-          const progress = (xhr.loaded / xhr.total) * 100;
-          // eslint-disable-next-line no-console
-          console.log(`Model loading progress: ${progress}%`);
-        },
-        (error) => {
-          // eslint-disable-next-line no-console
-          console.error(error);
-          reject(error);
-        },
-      );
-    });
-  }
-
-  static getBoundingObject(object) {
     BufferGeometry.prototype.computeBoundsTree = computeBoundsTree;
     BufferGeometry.prototype.disposeBoundsTree = disposeBoundsTree;
     Mesh.prototype.raycast = acceleratedRaycast;
+  }
 
-    if (object.name.endsWith(ModelLoader.boundingObjectSuffix)) {
-      return object;
-    }
+  async loadAsync(model) {
+    return new Promise((resolve, reject) => {
+      const group = new Group();
 
-    const boundingObject = object.clone();
-    boundingObject.updateMatrixWorld();
+      this.loader
+        .loadAsync(model.path)
+        .then((gltf) => gltf.scene)
+        .then((scene) => {
+          scene.traverse((modelChild) => {
+            const child = modelChild;
+            if (!(child instanceof Mesh)) {
+              return;
+            }
 
-    // Calculate a bounding object
-    boundingObject.name = `${object.name}${ModelLoader.boundingObjectSuffix}`;
-    boundingObject.material = new MeshBasicMaterial({ color: 0xff0000, opacity: 0.5, transparent: true });
+            if (ModelLoader.showBoundingBox) {
+              child.material = new MeshBasicMaterial({ color: 0xff0000, opacity: 0.5, transparent: true });
+            }
 
-    // Compute the bounds-tree to enable faster raycasting
-    boundingObject.traverse((child) => {
-      if (child instanceof Mesh && !ModelLoader.ignoreCollisionObjects.includes(child.name)) {
-        if (!child.geometry.boundsTree) {
-          child.geometry.computeBoundsTree();
-        }
-      }
+            if (!child.geometry.boundsTree) {
+              child.geometry.computeBoundsTree();
+            }
+
+            model.instances.forEach((instance, index) => {
+              const instanceMesh = child.clone();
+              instanceMesh.updateMatrixWorld();
+              if (instance.position) {
+                instanceMesh.position.copy(instance.position);
+              }
+              if (instance.rotation) {
+                instanceMesh.rotation.copy(instance.rotation);
+              }
+              if (instance.scale) {
+                instanceMesh.scale.copy(instance.scale);
+              }
+              if (instance.castShadow !== false) {
+                instanceMesh.castShadow = true;
+              }
+              if (instance.receiveShadow !== false) {
+                instanceMesh.receiveShadow = true;
+              }
+              if (instance.collidable !== false) {
+                instanceMesh.collidable = true;
+              }
+
+              instanceMesh.name = `${child.name}-${index}`;
+              group.add(instanceMesh);
+            });
+          });
+
+          resolve(group);
+        })
+        .catch((error) => {
+          reject(error);
+        });
     });
-
-    return boundingObject;
   }
 }
